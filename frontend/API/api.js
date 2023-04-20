97e24a0bb4a399fe09da580af6ecf5b109da950b
paleto30 <paleto0817@gmail.com>
2023-04-19 21:56:16 -0500

    feat ( endpoints ): :rocket: se agrego un archivo con los endpoints necesarios de momento , se agrego un logo y un form de buscar , falta funcionalidad

--- frontend/API/api.js
+++ frontend/API/api.js
@@ -1,9 +1,7 @@
 
 const key = "3df70b20cbd027249f00bb9372cbadf9";
-const genreId = 36;
-const url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=${genreId}`;
 
-
+const url = `https://api.themoviedb.org/3/movie/popular?api_key=3df70b20cbd027249f00bb9372cbadf9&page=1&language=es`;
 
 
 export const getAll =  async ()=>{
